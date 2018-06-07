b357b0d1c579612027680e203b83328b795e1863
Artur Lobanov <arturlbv@gmail.com>
2018-06-07 10:50:54 +0200

    Add options also to run_local.py
    Signed-off-by: Artur Lobanov <arturlbv@gmail.com>

--- run_local.py
+++ run_local.py
@@ -36,10 +36,15 @@ if __name__ == "__main__":
                       help="set to throw the data away")
     parser.add_option('-s','--showRawData',dest="showRawData",action="store_true",default=False,
                       help="set to decode and print raw data")
+    parser.add_option("-y", "--yaml", dest="yamlfname",type=str,action="store",
+                      help="yaml file to use",default="default-config.yaml")
+    parser.add_option("-m", "--moduleNumber", dest="moduleNumber",type=str,action="store",
+                      help="module number/name",default="test_board")
+
     (options, args) = parser.parse_args()
     print(options)
 
-    conf=yaml_config()
+    conf=yaml_config(options.yamlfname)
     conf.yaml_opt['daq_options']['acquisitionType'] = options.acquisitionType
     conf.yaml_opt['daq_options']['externalChargeInjection'] = options.externalChargeInjection
     conf.yaml_opt['daq_options']['injectionDAC'] = options.injectionDAC
@@ -86,7 +91,7 @@ if __name__ == "__main__":
 
     the_time=datetime.datetime.now()
     if glb_options['storeYamlFile'] and not options.dataNotSaved:
-        yamlFileName=glb_options['outputYamlPath']+"/Module"+str(glb_options['moduleNumber'])+"_"
+        yamlFileName=glb_options['outputYamlPath']+"/Module_"+options.moduleNumber+"_"
         yamlFileName=yamlFileName+str(the_time.day)+"-"+str(the_time.month)+"-"+str(the_time.year)+"_"+str(the_time.hour)+"-"+str(the_time.minute)
         yamlFileName=yamlFileName+".yaml"
         print("\t save yaml file : ",yamlFileName)
@@ -94,7 +99,7 @@ if __name__ == "__main__":
 
     outputFile=0
     if not options.dataNotSaved:
-        rawFileName=glb_options['outputRawDataPath']+"/Module"+str(glb_options['moduleNumber'])+"_"
+        rawFileName=glb_options['outputRawDataPath']+"/Module_"+options.moduleNumber+"_"
         rawFileName=rawFileName+str(the_time.day)+"-"+str(the_time.month)+"-"+str(the_time.year)+"_"+str(the_time.hour)+"-"+str(the_time.minute)
         rawFileName=rawFileName+".raw"
         print("\t open output file : ",rawFileName)
