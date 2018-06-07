f0a7b342f76fdc0e55bf73591c33fdab4d72f36e
Artur Lobanov <arturlbv@gmail.com>
2018-06-07 10:33:43 +0200

    Add yaml file path and module number as daq options

--- daq-zmq-client.py
+++ daq-zmq-client.py
@@ -6,18 +6,18 @@ from optparse import OptionParser
 
 class yaml_config:
     yaml_opt=yaml.YAMLObject()
-    
+
     def __init__(self,fname="default-config.yaml"):
         with open(fname) as fin:
             self.yaml_opt=yaml.safe_load(fin)
-            
+
     def dump(self):
         return yaml.dump(self.yaml_opt)
 
     def dumpToYaml(self,fname="config.yaml"):
         with open(fname,'w') as fout:
             yaml.dump(self.yaml_opt,fout)
-        
+
 def get_comma_separated_args(option, opt, value, parser):
     setattr(parser.values, option.dest, value.split(','))
 
@@ -37,17 +37,22 @@ if __name__ == "__main__":
                       help="set to true if you don't want to save the data (and the yaml file)")
     parser.add_option("-f", "--pulseDelay", dest="pulseDelay",type="int",action="store",
                       help="pulse delay (arbitrary unit) w.r.t. the trigger",default=72)
+    parser.add_option("-y", "--yaml", dest="yamlfname",type=str,action="store",
+                      help="yaml file to use",default="default-config.yaml")
+    parser.add_option("-m", "--moduleNumber", dest="moduleNumber",type=str,action="store",
+                      help="module number/name",default="test_board")
+
     (options, args) = parser.parse_args()
     print(options)
 
-    conf=yaml_config()
+    conf=yaml_config(options.yamlfname)
     conf.yaml_opt['daq_options']['acquisitionType']=options.acquisitionType
     conf.yaml_opt['daq_options']['externalChargeInjection']=options.externalChargeInjection
     conf.yaml_opt['daq_options']['injectionDAC']=options.injectionDAC
     conf.yaml_opt['daq_options']['pulseDelay']=options.pulseDelay
     for i in options.channelIds:
         conf.yaml_opt['daq_options']['channelIds'].append(int(i))
-    
+
     daq_options=conf.yaml_opt['daq_options']
     glb_options=conf.yaml_opt['glb_options']
 
@@ -55,7 +60,7 @@ if __name__ == "__main__":
 
     if glb_options['startServerManually']==False:
         os.system("ssh -T pi@"+glb_options['serverIpAdress']+" \"nohup python "+glb_options['serverCodePath']+"/daq-zmq-server.py > log.log 2>&1& \"")
-    
+
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
     print("Send request to server")
@@ -77,12 +82,12 @@ if __name__ == "__main__":
     if daq_options['compressRawData']==True:
             dataSize=15394 # 30784/2 + 2 for injection value
     dataStringUnpacker=struct.Struct('B'*dataSize)
-        
+
     outputFile=0
     if options.dataNotSaved==False:
         while True:
             the_time=datetime.datetime.now()
-            rawFileName=glb_options['outputRawDataPath']+"/Module"+str(glb_options['moduleNumber'])+"_"
+            rawFileName=glb_options['outputRawDataPath']+"/Module_"+options.moduleNumber+"_"
             rawFileName=rawFileName+str(the_time.day)+"-"+str(the_time.month)+"-"+str(the_time.year)+"_"+str(the_time.hour)+"-"+str(the_time.minute)
             rawFileName=rawFileName+".raw"
             if os.path.exists(rawFileName):
@@ -91,13 +96,13 @@ if __name__ == "__main__":
                 print("open output file : ",rawFileName)
                 outputFile = open(rawFileName,'wb')
                 if glb_options['storeYamlFile']==True:
-                    yamlFileName=glb_options['outputYamlPath']+"/Module"+str(glb_options['moduleNumber'])+"_"
+                    yamlFileName=glb_options['outputYamlPath']+"/Module_"+options.moduleNumber+"_"
                     yamlFileName=yamlFileName+str(the_time.day)+"-"+str(the_time.month)+"-"+str(the_time.year)+"_"+str(the_time.hour)+"-"+str(the_time.minute)
                     yamlFileName=yamlFileName+".yaml"
                     print("save yaml file : ",yamlFileName)
                     conf.dumpToYaml(yamlFileName)
                 break
-            
+
     cmd="CONFIGURE"
     print cmd
     socket.send(cmd)
@@ -108,7 +113,7 @@ if __name__ == "__main__":
     byteArray = bytearray(bitstring)
     if options.dataNotSaved==False:
         outputFile.write(byteArray)
-    
+
     #data_unpacker=unpacker.unpacker(daq_options['compressRawData'])
 
     # for i in xrange(0,daq_options['nEvent']):
@@ -144,10 +149,7 @@ if __name__ == "__main__":
                 socket.close()
                 context.term()
             break
-        
+
     except KeyboardInterrupt:
         print("keyboard interruption")
         os.system("ssh -T pi@"+glb_options['serverIpAdress']+" \" killall python\"")
-            
-
-
